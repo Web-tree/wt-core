49ac2e1476fd825d23b1430728b9119f40261bcb
lucifer <max.levicky@gmail.com>
2014-03-15 16:45:03 +0400

    WebTreeRuntimeException

---
+++ src/main/java/org/webtree/System/Exception/WebTreeRuntimeException.java
@@ -0,0 +1,9 @@
+package org.webtree.System.Exception;
+
+/**
+ * @author Max Levicky
+ *         Date: 15.03.14
+ *         Time: 16:37
+ */
+public class WebTreeRuntimeException extends RuntimeException {
+}
